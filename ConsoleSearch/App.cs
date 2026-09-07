7513229cf1e77fb8d49371020f373cf7dab561ae
Thor Frohn Smed Pedersen <166303113+ThorFSP@users.noreply.github.com>
2026-09-07 20:57:02 +0200

    Blazor and finishing the search app

--- ConsoleSearch/App.cs
+++ ConsoleSearch/App.cs
@@ -1,75 +1,31 @@
-﻿using System;
-using SearchSystem.Shared.Model;
-using Shared;
+﻿// ConsoleSearch/SearchClient.cs
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using Shared.Model;
 
 namespace ConsoleSearch
 {
-    public class App
+    public class SearchClient
     {
+        private readonly HttpClient mClient;
 
-        public void Run()
+        public SearchClient(string baseUrl = "http://localhost:5081")
         {
-            ISearchDatabase db = GetDatabase();
-            SearchLogic mSearchLogic = new SearchLogic(db);
-            Configs configs = new Configs();
-            Console.WriteLine("Console Search");
-            
-            while (true)
+            mClient = new HttpClient { BaseAddress = new System.Uri(baseUrl) };
+        }
+
+        public async Task<SearchResult> Search(string[] query, int maxAmount, bool caseSensitive)
+        {
+            var response = await mClient.PostAsJsonAsync("api/search", new
             {
-                Console.WriteLine("enter search terms - q for quit");
-                Console.WriteLine("type \"help\" for configs");
-                string input = Console.ReadLine();
-                if (input.Equals("q")) break;
-                if (input.Equals("help")) {
-                    GetConfigs(configs);
-                    continue;
-                }
-                if (input.Equals("case")) {
-                    // Toggle 
-                    configs.IsCaseSensitive = !configs.IsCaseSensitive;
-                    Console.WriteLine("IsCaseSensitive = " + configs.IsCaseSensitive);
-                    continue;
-                }
+                Query = query,
+                MaxAmount = maxAmount,
+                CaseSensitive = caseSensitive
+            });
 
-                var query = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-               
-
-                var result = mSearchLogic.Search(query, 10, configs.IsCaseSensitive);
-
-                if (result.Ignored.Count > 0) {
-                    Console.WriteLine($"Ignored: {string.Join(',', result.Ignored)}");
-                }
-                
-                int idx = 1;
-                foreach (var doc in result.DocumentHits) {
-                    Console.WriteLine($"{idx} : {doc.Document.mUrl} -- contains {doc.NoOfHits} search terms");
-                    Console.WriteLine("Index time: " + doc.Document.mIdxTime);
-                    Console.WriteLine($"Missing: {ArrayAsString(doc.Missing.ToArray())}");
-                    idx++;
-                }
-                Console.WriteLine("Documents: " + result.Hits + ". Time: " + result.TimeUsed.TotalMilliseconds);
-            }
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<SearchResult>();
         }
-        
-        private ISearchDatabase GetDatabase()
-        {
-            Console.Write("Use SQLite (1) or Postgres (2) database?");
-            string input = Console.ReadLine();
-            if (input.Equals("1"))
-                return new Shared.DatabaseSqlite();
-            else if (input.Equals("2"))
-                return new Shared.DatabasePostgres();
-            Console.WriteLine("Wrong input - try again...");
-            return GetDatabase();
-        }
-
-        public void GetConfigs(Configs configs)
-        {
-            Console.WriteLine("Configs:");
-            Console.WriteLine("IsCaseSensitive = " + configs.IsCaseSensitive + " (type \"case\" to toggle)");
-        }
-
-        string ArrayAsString(string[] s) => s.Length == 0?"[]":$"[{String.Join(',', s)}]";
     }
 }
--- indexer/App.cs
+++ indexer/App.cs
@@ -2,65 +2,28 @@
 using System.Collections.Generic;
 using System.IO;
 using Shared;
-using System.Diagnostics;
 
 namespace Indexer
 {
-    public class App
-    {
-        public void Run()
-        {
+	public class App
+	{
+		public void Run()
+		{
+			var folder = new DirectoryInfo(Config.FOLDER);
 
-            IIndexDatabase db = GetDatabase();
-            Crawler crawler = new Crawler(db);
+			if (!folder.Exists)
+			{
+				Console.WriteLine($"Folder does not exist: {folder.FullName}");
+				return;
+			}
 
-            var root = new DirectoryInfo(Config.FOLDER);
+			Console.WriteLine($"Indexing .txt files in {folder.FullName}");
 
-            DateTime start = DateTime.Now;
+			IIndexDatabase database = new DatabaseSqlite();
+			var crawler = new Crawler(database);
+			crawler.IndexFilesIn(folder, new List<string> { ".txt" });
 
-            crawler.IndexFilesIn(root, new List<string> { ".txt" });
-
-            TimeSpan used = DateTime.Now - start;
-            Console.WriteLine("DONE! used " + used.TotalMilliseconds);
-
-            var all = db.GetAllWords();
-
-            Console.WriteLine($"Indexed {db.DocumentCounts} documents");
-            Console.WriteLine($"Number of different words: {all.Count}");
-            int count = 10;
-            Console.WriteLine($"The first {count} is:");
-            foreach (var p in all)
-            {
-                Console.WriteLine("<" + p.Key + ", " + p.Value + ">");
-                count--;
-                if (count == 0) break;
-            }
-
-            Console.WriteLine("Hvor mange ord vil du se - inddelt efter hyppighed?");
-            var numberOfWords = Console.ReadLine();
-
-            MostFrequentWords(int.Parse(numberOfWords), db);
-
-        }
-
-        private IIndexDatabase GetDatabase()
-        {
-            Console.Write("Use SQLite (1) or Postgres (2) database?");
-            string input = Console.ReadLine();
-            if (input.Equals("1"))
-                return new Shared.DatabaseSqlite();
-            else if (input.Equals("2"))
-                return new Shared.DatabasePostgres();
-            Console.WriteLine("Wrong input - try again...");
-            return GetDatabase();
-        }
-
-        public void MostFrequentWords(int rows, IIndexDatabase db)
-        {
-            var words = db.GetMostFrequent(rows);   
-
-            foreach (var word in words)
-                Console.WriteLine($"<{word.Name}> - {word.Frequency}");
-        }
-    }
+			Console.WriteLine($"Indexed {database.DocumentCounts} documents.");
+		}
+	}
 }
